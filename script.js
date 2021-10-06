e1bbbcfbd4dc96928c5848c09b78594d49c82c78
Diana <89095339+Dianapirs@users.noreply.github.com>
2021-10-06 11:04:01 +0300

    HW9

--- script.js
+++ script.js
@@ -1,23 +1,56 @@
-function toDoList () {
-    let list = document.querySelector('.wrapper');
-    list.style.display = 'block';
-
-    let taskList = document.querySelector('.tasks');
-    let field = document.querySelector('#field');
+function todoList() {
+    const h1 = document.createElement('h1');
+    h1.innerText = 'My ToDoList';
+    const form = document.createElement('form');
+    const input = document.createElement('input');
+    const btnOk = document.createElement('button');
+    const ul = document.createElement('ul');
+    btnOk.innerText = 'Add task';
+    form.appendChild(h1);
+    form.appendChild(input);
+    form.appendChild(btnOk);
+    form.appendChild(ul);
+    document.body.appendChild(form);
     
-    function addTask () {
-        let task = document.createElement('li');
-        task.classList.add('task');
-        let newTask = field.value;
-        task.append(newTask);
-        taskList.appendChild(task);
-    }
-    field.addEventListener("keypress", (keyPressed) => {
-        const keyEnter = 13;
-        if (keyPressed.which == keyEnter) {
-            addTask();
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        if(input.value) {
+            const li = document.createElement('li');
+            const checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            const span = document.createElement('span');
+            span.innerText = input.value;
+            const btnDelete = document.createElement('button');
+            btnDelete.innerText = 'Del';
+            btnDelete.classList.add('btnDelete');
+            li.appendChild(checkbox);
+            li.appendChild(span);
+            li.appendChild(btnDelete);
+            ul.appendChild(li);
+            span.addEventListener('click', function(){
+                const editor = prompt('Change the task');
+                if(editor) {
+                    span.innerText = editor;
+                }
+                
+            })
+            btnDelete.addEventListener('click', function(){
+                li.remove();
+            })
+            
         }
+    
+        input.value = '';
     })
+        
 }
 
-toDoList();
+todoList();
+
+
+
+
+
+
